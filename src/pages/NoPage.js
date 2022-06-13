94677019cae9abd375df43cc11bf208fb1f7a5c5
Dillon Hunt <90228106+Dillon-Hunt@users.noreply.github.com>
2022-06-13 20:30:21 +1000

    Styled 404 page.

--- src/pages/NoPage.js
+++ src/pages/NoPage.js
@@ -1,10 +1,11 @@
-
+import '../styles/NoPage.css'
 
 function NoPage() {
     return (
       <div className="NoPage">
   
-        <h1>404</h1>
+        <h1 className="NoPage__Heading">404</h1>
+        <h2 className="NoPage__Subheading">Hmm</h2>
   
       </div>
     );
