32858a5e46bf9aedfe38b09acb40ae5742d24196
Dillon Hunt <90228106+Dillon-Hunt@users.noreply.github.com>
2022-06-21 17:11:15 +1000

    Added ability to like / favorite posts.

--- src/components/MobilePost.js
+++ src/components/MobilePost.js
@@ -1,5 +1,11 @@
 import '../styles/MobilePost.css'
 
+import { Link } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { getDoc, arrayUnion, doc, arrayRemove, updateDoc } from 'firebase/firestore'
+
+import { database } from '../root/App'
+
 // Helper Functions
 function getTimeString(milliseconds) {
   let time = Math.floor(milliseconds / 31536000000) 
@@ -24,13 +30,38 @@ function getValueString(count) {
 function MobilePost(props) {
   let { post, user } = props
 
+  const [favorite, setFavorite] = useState(false)
+  const [favoriteCount, setFavoriteCount] = useState(0)
+
+
+  useEffect(() => {
+    let bind = true
+    bind && getDoc(doc(database, 'favorites', post.id)).then(async document => {
+      if (!document.exists()) return () => bind = false
+      let favorites = await document.data()
+      setFavorite(favorites.users.includes(user.id))
+      setFavoriteCount(favorites.users.length)
+      return () => bind = false
+    })
+  }, [post, user])
+
+  const favoritePost = () => {
+    !favorite ? updateDoc(doc(database, 'favorites', post.id), {"users": arrayUnion(user.id)}).then(() => {
+      setFavoriteCount(favoriteCount + 1)
+      setFavorite(true)
+    }) : updateDoc(doc(database, 'favorites', post.id), {"users": arrayRemove(user.id)}).then(() => {
+      setFavoriteCount(favoriteCount - 1)
+      setFavorite(false)
+    })
+  }
+
   return (
     <div className="MobilePost">
       <div className="MobilePost__ProfileSection">
-        <div className='MobilePost__ImageContainer'>
+        <Link to={`/users/${post.data.user.username.toLowerCase()}`} className='MobilePost__ImageContainer'>
           <img className="MobilePost__ProfileIcon" src={post.data.user.profileIcon} alt='' />
-        </div>
-        <p className="MobilePost__Username">{post.data.user.name}</p>
+        </Link>
+        <Link to={`/users/${post.data.user.username.toLowerCase()}`}><p className="MobilePost__Username">{post.data.user.name}</p></Link>
         <p className="MobilePost__Time">{getTimeString(Math.floor((Date.now() - new Date(post.data.time))))}</p>
       </div>
 
@@ -38,8 +69,9 @@ function MobilePost(props) {
         <img className="MobilePost__Image" src={post.data.images[0]} alt="" /> {/* Add Slideshow In Future */}
         <div className="MobilePost__Overlay">
           <div className="icon25"> {/* post.id does not exist at the moment, will need a way to get the document id */}
-            <img className="MobilePost__Overlay__Icon" src={`../../assets/favorite${user.data.favorites.includes(post.id) ? "-filled" : ""}.svg`} alt="favorite" />
-            <p className="MobilePost__Overlay__Likes">{getValueString(post.data.favorites.length)}</p>
+            <img className="MobilePost__Overlay__Icon" src={`../../assets/favorite${favorite ? "-filled" : ""}.svg`} alt="favorite" />
+            <p className="MobilePost__Overlay__Likes">{getValueString(favoriteCount)}</p>
+            <button onClick={favoritePost} className="MobilePost__Overlay__Likes__Button" />
           </div>
 
 
